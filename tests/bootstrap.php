167bb27132fad25d4c5f05da236ae06e3ee96600
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-04-08 09:27:37 +0900

    remove deprecated annotation loader

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-/**
- * This file is part of the BEAR.AuraRouterModule package.
- *
- * @license http://opensource.org/licenses/MIT MIT
- */
-$loader = require \dirname(__DIR__) . '/vendor/autoload.php';
-/* @var $loader \Composer\Autoload\ClassLoader */
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
